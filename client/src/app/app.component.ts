4cfa078896b9539718a459303642193525cfc07e
Abhinav061 <abhinav061jyoti@GMAIL.COM>
2022-12-11 16:16:44 +0530

    CRUD Added

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -11,26 +12,55 @@ export class AppComponent implements OnInit {
   constructor(private httpClient: HttpClient){}
 
   title = 'client';
-  // Node = [
-  //   {
-  //     "id" : 1,
-  //     "name" : "Mark"
-  //   },
-  //   {
-  //     "id" : 2,
-  //     "name" : "Markus"
-  //   }
-  // ]
-
   Node: any;
 
+  form = new FormGroup({
+    Name: new FormControl(''),
+   });
+
   ngOnInit() {
-  this.getNodes();
+  this.getAllNodes();
   }
-  getNodes(){
+
+  getAllNodes(){
     this.httpClient.get('/get-all-node').subscribe((res)=>{
-      console.log(res);
+      console.log(res, 'All Node Response');
       this.Node =res
     })
   }
+  
+  getSindleNodes(id:any){
+    this.httpClient.get(`/get-single-node/${id}`).subscribe((res)=>{
+      console.log(res, 'Single Node Response');
+    })
+  }
+
+  deleteSingleNode(id:any){
+    this.httpClient.delete(`/delete-single-node/${id}`).subscribe((res)=>{
+      console.log(res, 'delete Response');
+      this.getAllNodes();
+    })
+  }
+
+  insertSingleNode(){
+    const req = {
+      "id" : "",
+      "name" : this.form.controls.Name.value
+    }
+    this.httpClient.post(`/insert-single-node`,req).subscribe((res)=>{
+      console.log(res, 'Insert Response');
+      this. getAllNodes();
+    })
+  }
+
+  updateSingleNode(){
+    const req = {
+      "id" : "",
+      "name" : ""
+    }
+    this.httpClient.put(`/update-node-with-id`,req).subscribe((res)=>{
+      console.log(res, 'Update Response');
+    })
+  }
+
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -14,6 +15,7 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule
     
   ],
