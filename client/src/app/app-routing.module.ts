904cbc894fef3ee22a8f710fdcfd0e1d462224b6
Abhinav061 <abhinav061jyoti@GMAIL.COM>
2022-12-15 20:28:19 +0530

    LoggedIn guard

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './shared/guards/auth.guard';
+import { LoggedinGuard } from './shared/guards/loggedin.guard';
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./components/login/login.module').then(m => m.LoginModule)
+    loadChildren: () => import('./components/login/login.module').then(m => m.LoginModule),
+    canActivate: [LoggedinGuard]
   },
   {
     path: 'login',
-    loadChildren: () => import('./components/login/login.module').then(m => m.LoginModule)
+    loadChildren: () => import('./components/login/login.module').then(m => m.LoginModule),
+    canActivate: [LoggedinGuard]
   },
   {
     path: 'home',
