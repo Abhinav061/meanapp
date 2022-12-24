b9de5a3314c7330dc2e39a06bd9b54239d7388c7
Abhinav061 <abhinav061jyoti@GMAIL.COM>
2022-12-24 20:07:35 +0530

    Update mailer.js

--- server/lib/mailer.js
+++ server/lib/mailer.js
@@ -14,13 +14,13 @@ let transporter = nodemailer.createTransport({
 
 module.exports = {
     async sendOptInMail(email, userID, token) {
-      let activationLink = `https://www.mean.w3designing.com/api/user/verify/${userID}/${token}`;
+      let activationLink = `http://www.mean.w3designing.com/api/user/verify/${userID}/${token}`;
       let mail = {
         from: '<admin@mean.w3designing.com>',
         to: email,
         subject: "Please active your account",
         text: `To activate your account, please click this link: ${activationLink}`,
-        html: `<p>To activate your account, please click this link: <a href="${activationLink}">${activationLink}</a></p>`,
+        html: `<p>To activate your account, <a href="${activationLink}">Click here</a></p>`,
       };
       await transporter.sendMail(mail);
     },
